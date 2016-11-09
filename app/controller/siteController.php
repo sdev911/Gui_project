5621fbec9a724a4e4bd9607a7003f3de79de135a
Allison Collier <allisc9@vt.edu>
2016-11-09 13:20:07 -0500

    Create Profile Page
    Added profile.tpl and updated files with connecting info.

--- app/controller/siteController.php
+++ app/controller/siteController.php
@@ -40,6 +40,10 @@ class SiteController {
 		 		$this->cart();
 				break;
 
+		case 'profile':
+				$this->profile();
+				break;
+
 			// redirect to home page if all else fails
       default:
         header('Location: '.BASE_URL);
@@ -53,6 +57,13 @@ class SiteController {
 		$pageName = 'Home';
 		include_once SYSTEM_PATH.'/view/header.tpl';
 		include_once SYSTEM_PATH.'/view/home.tpl';
+		include_once SYSTEM_PATH.'/view/footer.tpl';
+  }
+
+	public function profile() {
+		$pageName = 'Profile';
+		include_once SYSTEM_PATH.'/view/header.tpl';
+		include_once SYSTEM_PATH.'/view/profile.tpl';
 		include_once SYSTEM_PATH.'/view/footer.tpl';
   }
 
