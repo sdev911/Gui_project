0cf9457f56e1f254b75233e5fea51f1b1ba1f856
Allison Collier <allisc9@vt.edu>
2016-11-14 07:40:07 -0500

    Update profile.js

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     $('.profileForm').hide();
     $('.profileInfo').show();
     
-    $(.editProfile).click(function(){
+    $('.editProfile').click(function(){
       $('.profileInfo').hide();
       $('.profileForm').show();
     });
