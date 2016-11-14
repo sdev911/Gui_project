7b9f353da715a857c3d9de2835199fdd6a42f2f6
juliae13 <julia.evans94@gmail.com>
2016-11-14 13:17:23 -0500

    Added isFollowing method

--- app/model/Follower.class.php
+++ app/model/Follower.class.php
@@ -96,5 +96,17 @@ class Follower extends DbObject {
       $db = Db::instance();
       $db->execute($query);
     }
+    
+    public function isFollowing($follower_id, $following_id)
+    {
+      $query = sprintf(" SELECT id FROM %s WHERE follower_id=%d AND following_id=%d ORDER BY date_created DESC ", self::DB_TABLE, $follower_id, $following_id);
+      $db = Db::instance();
+      $result = $db->lookup($query);
+      if(!mysql_num_rows($result))
+          return false;
+      else {
+          return true;
+          }
+    }
 
 }
