756c43e3950501baef53ed3be29c952f87e74561
wizardwusa <andrew95@vt.edu>
2016-12-08 05:16:23 -0500

    Removed hash conversion, signup generates hash

--- app/controller/siteController.php
+++ app/controller/siteController.php
@@ -188,17 +188,13 @@ class SiteController {
 		$q = sprintf("SELECT * FROM user");
 		$result = mysql_query($q);
 		while($row = mysql_fetch_assoc($result)) {
-			if ($p == $row['password'] && $u ==$row['username'] /*&& password_verify($p, $row['password_hash'])*/)
+			if ($u ==$row['username'] && password_verify($p, $row['password_hash']))
 				{
 					$_SESSION['user'] = $row['username'];
 					$_SESSION['id'] = $row['id'];
 					$_SESSION['permissions']= $row['user_type'];
 					$_SESSION['msg'] = null;
 					header('Location: '.BASE_URL.'/');
-
-					$user = User::loadById($row['id']);
-					$user->set('password_hash', password_hash($p, PASSWORD_DEFAULT));
-					$user->save();
 
 					exit();
 				}
