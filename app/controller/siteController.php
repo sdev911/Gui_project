ccdbcc1f2a27e3e7921f9c59e998e85fa9f0bf70
wizardwusa <andrew95@vt.edu>
2016-12-09 22:49:12 -0500

    messages

--- app/controller/siteController.php
+++ app/controller/siteController.php
@@ -398,7 +398,7 @@ private function nextCatFact(){
 	public function conversation($userId){
 		// $messages = Messages::getMessages($_SESSION['id'], $userId); // loads all associated messages
 		include_once SYSTEM_PATH.'/view/header.tpl';
-		// include_once SYSTEM_PATH.'/view/messages.tpl';
+		include_once SYSTEM_PATH.'/view/messages.tpl';
 		include_once SYSTEM_PATH.'/view/footer.tpl';
 	}
 
