a34a467644d3124d2f8bec8df00c222278e512ac
wizardwusa <andrew95@vt.edu>
2016-12-09 22:35:30 -0500

    message fix

--- app/controller/siteController.php
+++ app/controller/siteController.php
@@ -396,7 +396,7 @@ private function nextCatFact(){
 	}
 
 	public function conversation($userId){
-		$comments = Messages::getMessages($_SESSION['id'], $userId); // loads all associated messages
+		$messages = Messages::getMessages($_SESSION['id'], $userId); // loads all associated messages
 		include_once SYSTEM_PATH.'/view/header.tpl';
 		include_once SYSTEM_PATH.'/view/messages.tpl';
 		include_once SYSTEM_PATH.'/view/footer.tpl';
